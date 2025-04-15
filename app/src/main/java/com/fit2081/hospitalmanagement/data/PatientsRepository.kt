1581dfb9d2463c75e56f791ad1594f875626bccc
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-04-15 12:20:21 +1000

    create repository to mediate between room db and viewmodel

--- app/src/main/java/com/fit2081/hospitalmanagement/data/PatientsRepository.kt
+++ app/src/main/java/com/fit2081/hospitalmanagement/data/PatientsRepository.kt
@@ -1,4 +1,31 @@
 package com.fit2081.hospitalmanagement.data
 
+import android.content.Context
+import kotlinx.coroutines.flow.Flow
+
+// Repository, mediator between Room Database and ViewModel
 class PatientsRepository {
+    var patientDao: PatientDao
+
+    constructor(context: Context){
+        patientDao = HospitalDatabase.getDatabase(context).patientDao()
+    }
+
+    suspend fun insert(patient: Patient){
+        patientDao.insert(patient)
+    }
+
+    suspend fun delete(patient: Patient){
+        patientDao.delete(patient)
+    }
+
+    suspend fun update(patient: Patient){
+        patientDao.update(patient)
+    }
+
+    suspend fun deleteAllPatients() {
+        patientDao.deleteAllPatients()
+    }
+
+    fun getAllPatients(): Flow<List<Patient>> = patientDao.getAllPatients()
 }
