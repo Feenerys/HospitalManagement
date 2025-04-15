113ee6a0e6115a732fd23211e4c3406cd94cda42
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-04-15 16:57:30 +1000

    add title to view and fix super.onCreate(savedInstanceState) being on the wrong line

--- app/src/main/java/com/fit2081/hospitalmanagement/MainActivity.kt
+++ app/src/main/java/com/fit2081/hospitalmanagement/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,29 +14,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.fit2081.hospitalmanagement.data.PatientViewModel
 import com.fit2081.hospitalmanagement.ui.theme.HospitalManagementTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Obtains an instance of the ViewModel
-        val viewModel: PatientViewModel = ViewModelProvider(
-            this, PatientViewModel.PatientViewModelFactory(this@MainActivity)
-        )[PatientViewModel::class.java]
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             HospitalManagementTheme {
+                // Obtains an instance of the ViewModel
+                val viewModel: PatientViewModel = ViewModelProvider(
+                    this, PatientViewModel.PatientViewModelFactory(this@MainActivity)
+                )[PatientViewModel::class.java]
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    // obtains the total number of patients as a state, gets updated every change
-                    val numberOfPatients by viewModel.allPatients.collectAsState(initial = emptyList())
+                    AddPatientScreen(innerPadding, viewModel)
                 }
             }
         }
@@ -41,17 +46,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AddPatientScreen(innerPadding: PaddingValues, viewModel: PatientViewModel) {
+    var patientName by remember { mutableStateOf(TextFieldValue(""))}
+    var patientAge by remember {mutableStateOf(TextFieldValue(""))}
+    var patientAddress by remember {mutableStateOf(TextFieldValue(""))}
+    // Use state so that when any change in data occurs the list is updated
+    val numberOfPatients by viewModel.allPatients.collectAsState(initial = emptyList())
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HospitalManagementTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "Hospital Management",
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+
     }
+
 }
