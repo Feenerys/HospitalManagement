5c0acecd908ab142b7b85dbb5680736544ca6584
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-04-15 12:19:41 +1000

    remove unnecessary dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.constraintlayout)
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
---
+++ app/src/main/java/com/fit2081/hospitalmanagement/data/PatientsRepository.kt
@@ -0,0 +1,4 @@
+package com.fit2081.hospitalmanagement.data
+
+class PatientsRepository {
+}
